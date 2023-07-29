7d47575fc2bb370068c6fa16f8921aac4ffe8219
karosuo <rafaelkarosuo@gmail.com>
2023-07-28 22:52:40 -0700

    Fix, UART.c, agregar volatile a la direccion base de UART

--- src/UART.c
+++ src/UART.c
@@ -8,9 +8,10 @@ void UART_Init(UART_DATA_t * uart_peripheral)
     {
         enable_rx_int = true;
     }    
-    USART_t * USARTxREG = (USART_t *) USART_BASE_ADDR + (USART_PERIPH_OFFSET * uart_peripheral->number);
-    USARTxREG->BAUD = getScaledBaudRate(uart_peripheral->baud_rate);
-    USARTxREG->CTRLB |= USART_TXEN_bm; // Enable USART1    
+    // Calculate the selected UART address
+    volatile USART_t * USARTxREG = (USART_t *) (USART_BASE_ADDR + (USART_PERIPH_OFFSET * uart_peripheral->number));
+    USARTxREG->BAUD = getScaledBaudRate(uart_peripheral->baud_rate); // Assign baud rate
+    USARTxREG->CTRLB |= USART_TXEN_bm; // Enable selected UART
 }
 
 uint16_t public_getScaledBaudRate(uint32_t baud_rate)
